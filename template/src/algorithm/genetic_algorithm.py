5520e3dff29aca42f2b60116561f729dd65c4b38
Vitor Amorim <amorim.vitor.ds@gmail.com>
2021-08-11 22:14:55 -0300

    Add files via upload

---
+++ template/src/algorithm/genetic_algorithm.py
@@ -0,0 +1,13 @@
+
+def genetic_algorithm(problem, population_size, n_generations, mutation_rate=0.1):
+
+    # Create a initial population
+    ini_population = problem.initial_population(population_size)
+    best_fit, best_pos, path, fitn = problem.fitness(ini_population)
+
+    for i in range(0, 1):
+
+
+
+
+    return []
