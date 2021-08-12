0a3766b4b8eb5b19890ac9dd1a01ada905580b16
Vitor Amorim <amorim.vitor.ds@gmail.com>
2021-08-12 10:37:34 -0300

    Add files via upload

--- template/src/algorithm/genetic_algorithm.py
+++ template/src/algorithm/genetic_algorithm.py
@@ -1,3 +1,4 @@
+import random
 
 def genetic_algorithm(problem, population_size, n_generations, mutation_rate=0.1):
 
@@ -5,9 +6,15 @@ def genetic_algorithm(problem, population_size, n_generations, mutation_rate=0.1
     ini_population = problem.initial_population(population_size)
     best_fit, best_pos, path, fitn = problem.fitness(ini_population)
 
-    for i in range(0, 1):
+    list = []
+    for i in range(0, len(fitn)): list.append(i)
 
+    for j in range(0, len(fitn)):
 
+        # retorna a posição de onde os pais estão dentro da lista da população
+        pai1, pai2 = problem.selection_process(list, fitn)
 
+        # Realiza o Crossover - Order 1
+        problem.crossover(ini_population[pai1], ini_population[pai2])
 
     return []
