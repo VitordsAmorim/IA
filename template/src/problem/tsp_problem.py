22f2854314df17bed9345053f7ab53bca9872811
Vitor Amorim <amorim.vitor.ds@gmail.com>
2021-08-13 12:37:49 -0300

    Add files via upload

--- template/src/problem/tsp_problem.py
+++ template/src/problem/tsp_problem.py
@@ -1,6 +1,7 @@
 import numpy as np
-from src.problem.problem_interface import ProblemInterface
+from template.src.problem.problem_interface import ProblemInterface
 import random
+import matplotlib.pyplot as plt
 
 class TSPProblem(ProblemInterface):
 
@@ -18,18 +19,20 @@ class TSPProblem(ProblemInterface):
         pass
 
     def initial_population(self, population_size):
+
         # Write the list with the for 1 to the size of the population(the number of cities)
-        list = []
-        for i in range(1, len(self.x)): list.append(i)
+        lista = []
+        for i in range(1, len(self.x)):
+            lista.append(i)
 
         # The initial population has size 200
         population = []
-        for i in range(0, population_size):
-            population.append(random.sample(list, len(self.x) - 1))
+        for j in range(0, population_size):
+            population.append(random.sample(lista, len(self.x) - 1))
         return population
 
-
     def fitness(self, population):
+
         # Calcular a função fitness para toda população
         fitn = []
         for k in range(0, len(population)):
@@ -46,75 +49,113 @@ class TSPProblem(ProblemInterface):
             total_distance = total_distance + float(distance)
             fitn.append(float(total_distance))
 
-        best_fit = max(fitn)
+        best_fit = min(fitn)
         best_pos = fitn.index(best_fit)
-        path = []
         path = population[best_pos]
         return best_fit, best_pos, path, fitn
 
+    def elitism(self, newpopulation, bestindividualold, fitn):
 
-    def new_individual(self):
-        ###################################
-        # TODO
-        ###################################
-        individual = None
-        return individual
+        # Encontrar a pior solução, para depois
+        # substitui-la pela melhor solução da geração anterior
+        bigger_fitn = max(fitn)
+        pos = fitn.index(bigger_fitn)
+        newpopulation[pos] = bestindividualold
+        return newpopulation
 
-    def mutation(self, individual):
-        ###################################
-        # TODO
-        ###################################
+    def mutation(self, individual, mutation_rate):
+
+        lista = []
+        prob = np.random.random_sample()
+        if prob < mutation_rate:
+
+            # print("Individual before the mutation: ")
+            # print("    ", individual)
+
+            for i in range(0, len(individual)):
+                lista.append(i)
+
+            # Randomly select two positions
+            rand_pos1, rand_pos2 = random.sample(lista, 2)
+
+            # Swap the position of two cities
+            aux = individual[rand_pos1]
+            individual[rand_pos1] = individual[rand_pos2]
+            individual[rand_pos2] = aux
+
+            # print("Individual after the mutation: ")
+            # print("    ",individual)
         return individual
 
     def crossover(self, p1, p2):
-        print("pai1  ", p1)
-        print("\npai2  ", p2)
+        # print("pai1  ", p1)
+        # print("pai2  ", p2, "\n")
 
         son1 = [0] * len(p1)
         son2 = [0] * len(p1)
+
         # Randomly choose two points to slice a solution
-        list = []
-        for i in range(1, len(p1)): list.append(i)
-        corte1, corte2 = random.sample(list, 2)
+        lista = []
+        for i in range(1, len(p1)):
+            lista.append(i)
+        corte1, corte2 = random.sample(lista, 2)
 
         if corte1 > corte2:
             start_point = corte2
-            end_point   = corte1
+            end_point = corte1
         else:
             start_point = corte1
-            end_point   = corte2
+            end_point = corte2
 
+        # Return 1 son per function, then it's only necessary to flip p1 and p2
+        # to generate the second son
+        son1 = self.subcrossover(start_point, end_point, p1, p2, son1)
+        # print("filho ",son1)
+        son2 = self.subcrossover(start_point, end_point, p2, p1, son2)
+        # print("filho ",son2)
+        return son1, son2
+
+    def subcrossover(self, start_point, end_point, p1, p2, son1):
+
+        # Write the gene of pai2 in the son1
         for i in range(start_point, end_point):
             son1[i] = p2[i]
-        print("filho1", son1)
 
+        # Write the gene of pai1 in the son1 - Part 1
         j = end_point
         end_list = len(p1)
         for i in range(end_point, end_list):
 
-            if i == (len(p1) - 1):
-                end_point = 0
-                end_list  = start_point
-                i = 0
-            elif p1[i] not in son1:
+            if p1[i] not in son1:
                 son1[j] = p1[i]
                 j = j + 1
 
             if j == len(p1):
                 j = 0
-            print("filho1", son1)
 
-        print("filho1", son1)
+        # Write the gene of pai1 in the son1 - Part 2
+        for i in range(0, end_list):
 
-        """for i in range(0, start_point):
-            son1[i] = p1[i]
-        print("filho1", son1)"""
+            if p1[i] not in son1:
+                son1[j] = p1[i]
+                j = j + 1
 
-        return son1, son2
+            if j == start_point:
+                break
+            elif j == len(p1):
+                j = 0
+        return son1
 
-    def selection_process(self, list, fitn):
+    def selection_process(self, fitn):
 
-        i1, i2 = random.sample(list, 2)
+        lista = []
+        for i in range(0, len(fitn)):
+            lista.append(i)
+
+        # Escolhe aleatoriamente dois individuos para competirem
+        # e o que tiver menor valor de fitness vence, tornando-se
+        # o pai 1
+        i1, i2 = random.sample(lista, 2)
         # Selection process - It want to minimize
         if fitn[i1] > fitn[i2]:
             pai1 = i2
@@ -122,17 +163,43 @@ class TSPProblem(ProblemInterface):
             pai1 = i1
         else:
             pai1 = i1  # depois colocar uma escolha aleatória caso algo de errado
-        i1, i2 = random.sample(list, 2)
+
+        # Escolhe aleatoriamente outros dois individuos para competirem
+        # e o que tiver menor valor de fitness vence, tornando-se
+        # o pai 2
+        i1, i2 = random.sample(lista, 2)
         if fitn[i1] > fitn[i2]:
             pai2 = i2
         elif fitn[i1] < fitn[i2]:
             pai2 = i1
         else:
-            pai1 = i1
+            pai2 = i1
         return pai1, pai2
 
-    def plot(self, individual):
-        ###################################
-        # TODO
-        ###################################
+    def plot(self, best_fitness, ngeracoes):
+
+        best_fitness = np.asarray(best_fitness)
+        ngeracoes = np.asarray(ngeracoes)
+        x = ngeracoes
+        y = best_fitness
+        plt.plot(x, y)
+        plt.title("TSP - 30 cities")
+        plt.xlabel("Nº Gerações")
+        plt.ylabel("Fitness")
+        plt.show()
+        pass
+
+    def plot_bestfit(self, best_individual):
+        xp, yp = [], []
+        best_individual.insert(0, 0)
+        best_individual.insert(len(best_individual), 0)
+        for k in range(0, len(best_individual)):
+            indice = best_individual[k]
+            xp.append(float(self.x[indice]))
+            yp.append(float(self.y[indice]))
+
+        x = np.asarray(xp)
+        y = np.asarray(yp)
+        plt.plot(x, y, marker="o", markerfacecolor="r")
+        plt.show()
         pass
