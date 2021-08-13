fcd3d2adf16f3fe01a3c62de3f2f447693dfa882
Vitor Amorim <amorim.vitor.ds@gmail.com>
2021-08-13 12:38:25 -0300

    Add files via upload

--- template/src/algorithm/genetic_algorithm.py
+++ template/src/algorithm/genetic_algorithm.py
@@ -1,20 +1,47 @@
-import random
 
 def genetic_algorithm(problem, population_size, n_generations, mutation_rate=0.1):
 
     # Create a initial population
-    ini_population = problem.initial_population(population_size)
-    best_fit, best_pos, path, fitn = problem.fitness(ini_population)
+    old_population = problem.initial_population(population_size)
+    best_fit, best_pos, best_individual, fitn = problem.fitness(old_population)
 
-    list = []
-    for i in range(0, len(fitn)): list.append(i)
+    # Armazena o melhor indivíduo de cada uma das gerações
+    best_solutions = []
 
-    for j in range(0, len(fitn)):
+    ngeracoes = []
+    for k in range(0, n_generations):
+        # 'zerar' os valores da nova população
+        new_population = []
+        for m in range(0, (population_size//2)):
 
-        # retorna a posição de onde os pais estão dentro da lista da população
-        pai1, pai2 = problem.selection_process(list, fitn)
+            # seleciona dois pais dentro da população
+            # retorna a posição de onde os pais estão dentro da lista da população
+            # fitn contain all the fitness of the population
+            pai1, pai2 = problem.selection_process(fitn)
 
-        # Realiza o Crossover - Order 1
-        problem.crossover(ini_population[pai1], ini_population[pai2])
+            # Realiza o Crossover - Order 1
+            # Retorna 2 indivíduos da próxima geração
+            # individual one and two (i1, i2)
+            i1, i2 = problem.crossover(old_population[pai1], old_population[pai2])
 
+            f1 = problem.mutation(i1, mutation_rate)
+            f2 = problem.mutation(i2, mutation_rate)
+            new_population.append(f1)
+            new_population.append(f2)
+
+        # ELITISMO
+        # Faz com que o melhor indivíduo da geraçao passada sobreviva para a nova
+        # geracao, substituindo o pior individuo da geraçao atual
+        best_solutions.append(best_fit)
+
+        # Foi necessário por esse if, porque em alguns casos, o melhor indivíduo
+        # mesmo sobrevivendo e sendo inserido na nova população, ele não foi escolhido
+        # para gerar filhos no torneio
+        best_individual_old = best_individual
+        old_population = problem.elitism(new_population, best_individual_old, fitn)
+        best_fit, best_pos, best_individual, fitn = problem.fitness(new_population)
+        ngeracoes.append(k)
+
+    problem.plot(best_solutions, ngeracoes)
+    problem.plot_bestfit(best_individual)
     return []
