b6c0eb1d88934a2f3866b4957f5baf3bb497a92a
Vitor Amorim <amorim.vitor.ds@gmail.com>
2021-08-15 17:59:16 -0300

    Add files via upload

--- template/main.py
+++ template/main.py
@@ -4,12 +4,14 @@ from src.problem.regression_problem import RegressionProblem
 from src.problem.classification_problem import ClassificationProblem
 from src.problem.tsp_problem import TSPProblem
 from src.algorithm.genetic_algorithm import genetic_algorithm
+import time
 
 
-def generate_report(output):
-    ###################################
-    # TODO
-    ###################################
+def generate_report(output, problem, n_generations, best_fitness):
+    problem.plot(output, n_generations)
+    menorfit = min(best_fitness)
+    meiorfit = max(best_fitness)
+    media = sum(best_fitness)/n_generations
     pass
 
 
@@ -19,7 +21,7 @@ def build_problem(problem_name):
     elif problem_name == "regression":
         return RegressionProblem("data/regression/data-3.txt")
     elif problem_name == "tsp":
-        return TSPProblem("data/tsp/tsp-10.txt")
+        return TSPProblem("data/tsp/tsp-30.txt")
     else:
         raise NotImplementedError()
 
@@ -31,27 +33,31 @@ def read_command_line_args():
     parser.add_argument('-p', '--problem', default='tsp',
                         choices=["classification", "regression", "tsp"])
     parser.add_argument('-n', '--n_generations', type=int,
-                        default=10, help='number of generations.')
+                        default=100, help='number of generations.')
     parser.add_argument('-s', '--population_size', type=int,
-                        default=20, help='population size.')
+                        default=50, help='population size.')
     parser.add_argument('-m', '--mutation_rate', type=float,
-                        default=0.1, help='mutation rate.')
+                        default=0.2, help='mutation rate.')
 
     args = parser.parse_args()
     return args
 
 
 def main():
-    args = read_command_line_args()
 
+    args = read_command_line_args()
     problem = build_problem(args.problem)
 
-    output = genetic_algorithm(
-        problem,
-        population_size=args.population_size,
-        n_generations=args.n_generations)
-
-    generate_report(output)
+    graph, best_fitness = [], []
+    for i in range(0, 5):
+        output, n = genetic_algorithm(
+            problem,
+            population_size=args.population_size,
+            n_generations=args.n_generations,
+            round=i)
+        graph.append(output)
+        best_fitness.append(output[args.n_generations - 1])
+    generate_report(graph, problem, n, best_fitness)
 
     print("OK!")
 
