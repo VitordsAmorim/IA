a9437de6ce4cc3f30c37cd813fb445a3906a0a5e
Vitor Amorim <amorim.vitor.ds@gmail.com>
2021-08-15 14:15:47 -0300

    Add files via upload

--- template/src/algorithm/genetic_algorithm.py
+++ template/src/algorithm/genetic_algorithm.py
@@ -3,12 +3,14 @@ def genetic_algorithm(problem, population_size, n_generations, mutation_rate=0.1
 
     # Create a initial population
     old_population = problem.initial_population(population_size)
+    # best_fit: valor da fitnes do melhor indivíduo
+    # best_pos: posicao dentro da população, em que está o melhor indivíduo
+    # path: os valores, dos parametros, do melhor indivíduo
+    # fitn: o valor da fitnes de toda a população
     best_fit, best_pos, best_individual, fitn = problem.fitness(old_population)
 
     # Armazena o melhor indivíduo de cada uma das gerações
-    best_solutions = []
-
-    ngeracoes = []
+    best_solutions, ngeracoes = [], []
     for k in range(0, n_generations):
         # 'zerar' os valores da nova população
         new_population = []
@@ -32,16 +34,12 @@ def genetic_algorithm(problem, population_size, n_generations, mutation_rate=0.1
         # ELITISMO
         # Faz com que o melhor indivíduo da geraçao passada sobreviva para a nova
         # geracao, substituindo o pior individuo da geraçao atual
+        print("Iterations:", k, "best fitness:", best_fit)
         best_solutions.append(best_fit)
-
-        # Foi necessário por esse if, porque em alguns casos, o melhor indivíduo
-        # mesmo sobrevivendo e sendo inserido na nova população, ele não foi escolhido
-        # para gerar filhos no torneio
         best_individual_old = best_individual
         old_population = problem.elitism(new_population, best_individual_old, fitn)
         best_fit, best_pos, best_individual, fitn = problem.fitness(new_population)
         ngeracoes.append(k)
-
     problem.plot(best_solutions, ngeracoes)
     problem.plot_bestfit(best_individual)
     return []
